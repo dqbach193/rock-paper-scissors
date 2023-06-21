40051231a61ee9b9c7f08d844275070cdb3a613a
yeutv <dqbach193@gmail.com>
2023-06-21 21:08:27 +0700

    add simple UI to the game

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const rock = document.querySelector('#rock')
+const paper = document.querySelector('#paper')
+const scissors = document.querySelector('#scissors')
+const resultDiv = document.querySelector('#result')
+const finalResult = document.querySelector('#finalResult')
+
 function getComputerChoice(){
     //Choose a random number between 0 and 99
     let num = Math.floor(Math.random() * 100);
@@ -40,6 +46,47 @@ function playRound(playerSelection, computerSelection){
         }else{
             return 'You win! Paper lose to Scissors';
         }
+    }
+}
+
+let userScore = 0;
+let compScore = 0;
+
+rock.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
+    let result = playRound('rock',computerSelection);
+    calScore(result);
+    resultDiv.innerHTML = result + `<br>` + ` Current Score: User Score: ${userScore}, Computer Score: ${compScore}`;
+})
+
+paper.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
+    let result = playRound('paper',computerSelection);
+    calScore(result);
+    resultDiv.innerHTML = result + `<br>` + ` Current Score: User Score: ${userScore}, Computer Score: ${compScore}`;
+})
+
+scissors.addEventListener('click', () => {
+    let computerSelection = getComputerChoice();
+    let result = playRound('scissors',computerSelection);
+    calScore(result);
+    resultDiv.innerHTML = result + `<br>` + ` Current Score: User Score: ${userScore}, Computer Score: ${compScore}`;
+})
+
+function calScore(result){
+    if (result.charAt(4) === "l"){
+        compScore = compScore + 1;
+    }else if(result.charAt(4) === "w"){
+        userScore = userScore + 1;
+    }
+    finalScore();
+}
+
+function finalScore(){
+    if(compScore == 5 && userScore < 5){
+        finalResult.innerHTML += 'You Lost! Better luck next time';
+    }else if(userScore == 5){
+        finalResult.innerHTML += 'You Won! Congratulations :D';
     }
 }
 
